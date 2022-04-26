d08336e24bd9e58576c3631f69d158b412c9a4d9
Facundo <facundogoiriz@gmail.com>
2022-04-26 14:24:23 -0300

    new feature, Dark, Dark collectors, swaps between Gold and Dark

--- scripts/pygame_utils.py
+++ scripts/pygame_utils.py
@@ -4,7 +4,7 @@ import pygame
 def loop(player):
     import pygame
     pygame.init()
-    screen = pygame.display.set_mode((900, 800))
+    screen = pygame.display.set_mode((1100, 1000))
 
     BLACK = (0, 0, 0)
     GRAY = (200, 200, 200)
@@ -13,33 +13,43 @@ def loop(player):
     font = pygame.font.SysFont(None, 48)
 
     game_over = False
-    state = [0, 0, 0, 0]
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
+                if event.key == pygame.K_1:
                     player.buyLand()
+                if event.key == pygame.K_2:
+                    player.buyGold()
+                if event.key == pygame.K_3:
+                    player.buyDark()
+                elif event.key == pygame.K_q:
+                    player.buyGoldCollector()
                 elif event.key == pygame.K_w:
-                    player.buyCollector()
+                    player.buyDarkCollector()
                 elif event.key == pygame.K_e:
                     player.upgradeMultiplier()
                 elif event.key == pygame.K_r:
                     game_over = True
 
-        player.goldRefresh()
+        player.balances()
         state = player.display()
 
         screen.fill(background)
-        screen.blit(font.render("If you manage to gain 100000 Gold", True, BLACK), (50, 0))
-        screen.blit(font.render("and buy more than 10 Collectors you win", True, BLACK), (50, 30))
-        screen.blit(font.render("Q:     Buy Land  -- Cost 1 Ether", True, BLACK), (50, 100))
-        screen.blit(font.render("W:     Buy Collector  -- Cost 30 Gold", True, BLACK), (50, 150))
-        screen.blit(font.render("E:     Buy Multiplier  -- Cost 300 Gold", True, BLACK), (50, 200))
-        screen.blit(font.render("R:     Quit Game", True, BLACK), (50, 250))
-        screen.blit(font.render(f"Ether Balance: {state[0]}", True, BLACK), (50, 400))
-        screen.blit(font.render(f"Gold Balance: {state[1]}   ---   {state[2] * state[3]} Gold per second", True, BLACK), (50, 500))
-        screen.blit(font.render(f"Number of Collectors: {state[2]}", True, BLACK), (50, 600))
-        screen.blit(font.render(f"Collector Multiplier: {state[3]}x", True, BLACK), (50, 700))
+        screen.blit(font.render("to win gain 100000 Gold and 10000 Dark", True, BLACK), (50, 0))
+        screen.blit(font.render("buy +10 Collectors and +10 Dark Collectors", True, BLACK), (50, 50))
+        screen.blit(font.render("1:     Buy Land  -- Cost 1 Ether", True, BLACK), (50, 100))
+        screen.blit(font.render("2:     Buy Gold with Dark  -- Cost all your Dark", True, BLACK), (50, 150))
+        screen.blit(font.render("3:     Buy Dark with Gold  -- Cost all your Gold", True, BLACK), (50, 200))
+        screen.blit(font.render("Q:     Buy Collector  -- Cost 30 Gold", True, BLACK), (50, 250))
+        screen.blit(font.render("W:     Buy Dark Collector  -- Cost 50 Dark", True, BLACK), (50, 300))
+        screen.blit(font.render("E:     Buy Multiplier  -- Cost 300 Gold, 300 Dark", True, BLACK), (50, 350))
+        screen.blit(font.render("R:     Quit Game", True, BLACK), (50, 400))
+        screen.blit(font.render(f"Ether Balance: {state[0]}", True, BLACK), (50, 650))
+        screen.blit(font.render(f"Gold Balance: {state[1]}   ---   {state[3] * state[5]} Gold per second", True, BLACK), (50, 700))
+        screen.blit(font.render(f"Dark Balance: {state[2]}   ---   {(state[4] * state[5])/4} Dark per second", True, BLACK), (50, 750))
+        screen.blit(font.render(f"Number of Collectors: {state[3]}", True, BLACK), (50, 800))
+        screen.blit(font.render(f"Number of Dark Collectors: {state[4]}", True, BLACK), (50, 850))
+        screen.blit(font.render(f"Multiplier: {state[5]}x", True, BLACK), (50, 900))
         pygame.display.update()
         
         while player.win and not game_over:
--- scripts/tycoon_utils.py
+++ scripts/tycoon_utils.py
@@ -7,63 +7,97 @@ class Player:
         self.contract = contract
         self.payDic = payDic
         self.me = me
+        self.lastCheck = int(time.time())
+        self.factor = 100
         self.balance = 0
         self.gold = 0
-        self.collectors = 0
+        self.dark = 0
+        self.goldCollectors = 0
+        self.darkCollectors = 0
         self.multiplier = 0
-        self.lastCheck = int(time.time())
-        self.goldRate = 0
         self.win = False
 
     def buyLand(self):
         try:
             self.contract.buyLand({"from": self.me, "value": Wei("1 ether")})
-            self.refreshState()
-            self.goldRefresh()
+            self.lastCheck = int(time.time())
+            self.state()
+            self.balances()
         except:
             return 0
 
-    def buyCollector(self):
+    def buyGold(self):
         try:
-            self.contract.buyCollector(self.payDic)
-            self.refreshState()
-            self.goldRefresh()
+            self.contract.buyGold(self.payDic)
+            self.state()
+            self.balances()
+        except:
+            return 0
+
+    def buyDark(self):
+        try:
+            self.contract.buyDark(self.payDic)
+            self.state()
+            self.balances()
+        except:
+            return 0
+
+    def buyGoldCollector(self):
+        try:
+            self.contract.buyGoldCollector(self.payDic)
+            self.state()
+            self.balances()
+        except:
+            return 0
+
+    def buyDarkCollector(self):
+        try:
+            self.contract.buyDarkCollector(self.payDic)
+            self.state()
+            self.balances()
         except:
             return 0
 
     def upgradeMultiplier(self):
         try:
             self.contract.upgradeMultiplier(self.payDic)
-            self.refreshState()
-            self.goldRefresh()
+            self.state()
+            self.balances()
         except:
             return 0
 
     def display(self):
-        self.goldRefresh()
-        return self.balance, self.gold, self.collectors, self.multiplier
+        self.balances()
+        return self.balance, self.gold, self.dark, self.goldCollectors, self.darkCollectors, self.multiplier
 
-    def refreshState(self):
+    def state(self):
         try:
             self.contract.refresh()
+            self.lastCheck = int(time.time())
+            res = self.contract.display().return_value
             self.balance = str(round(float(self.me.balance().to("ether")), 4))
-            self.gold = int(self.contract.display().return_value[0])
-            self.collectors = int(self.contract.display().return_value[1])
-            self.multiplier = int(self.contract.display().return_value[2])
+            self.gold = int(res[0])/self.factor
+            self.dark = int(res[1])/self.factor
+            self.goldCollectors = int(res[2])
+            self.darkCollectors = int(res[3])
+            self.multiplier = int(res[4])
         except:
             self.balance = str(round(float(self.me.balance().to("ether")), 4))
-            self.gold = int(self.contract.display().return_value[0])
-            self.collectors = int(self.contract.display().return_value[1])
-            self.multiplier = int(self.contract.display().return_value[2])
+            self.gold = 0
+            self.dark = 0
+            self.goldCollectors = 0
+            self.darkCollectors = 0
+            self.multiplier = 0
 
-    def goldRefresh(self):
+    def balances(self):
         new_check = int(time.time())
         remainder = new_check - self.lastCheck
-        if self.gold >= 100000 and self.collectors >= 10:
+        if self.gold >= 100000 and self.dark >= 10000 and self.goldCollectors >= 10 and self.darkCollectors >= 10:
             self.win = True
-            self.refreshState()
+            self.state()
             return
         if remainder < 1:
             return
         self.lastCheck = new_check
-        self.gold += remainder * self.collectors * self.multiplier
+        self.gold += remainder * self.goldCollectors * self.multiplier
+        self.dark += (remainder * self.darkCollectors * self.multiplier) / 4
